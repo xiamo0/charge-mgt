971bac46fd664bb3a1744eca34ca0819bcd1b8be
Mo Lin <65656443+xiamo0@users.noreply.github.com>
2026-05-08 22:59:59 +0800

    Delete target directory

--- target/debug/deps/pin_project_lite-c108cd108480954a.d
+++
@@ -1,5 +0,0 @@
-/Users/molinyi/Documents/GitHub/charge-mgt/target/debug/deps/libpin_project_lite-c108cd108480954a.rmeta: /Users/molinyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pin-project-lite-0.2.17/src/lib.rs
-
-/Users/molinyi/Documents/GitHub/charge-mgt/target/debug/deps/pin_project_lite-c108cd108480954a.d: /Users/molinyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pin-project-lite-0.2.17/src/lib.rs
-
-/Users/molinyi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pin-project-lite-0.2.17/src/lib.rs:
